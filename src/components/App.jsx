65a19a98dbde957107bc68089fded2e9f877b433
Riley Benson <ryguybenson@yahoo.com>
2019-04-19 11:06:41 -0700

    add proptypes and initiate webpack dev server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import TicketList from "./TicketList";
 import Header from "./Header";
 
 function App(){
   return (
     <div>
       <Header/>
-      <h3>3a</h3>
-      <h3>Thato and Haley</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <TicketList/>
     </div>
   );
 }
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Ticket from "./Ticket";
 
 function TicketList(){
-  return ();
+  return (
+    <Ticket
+      location="3A"
+      names="Thato and Haley"
+      issue="Firebase will not save record!"/>
+  );
 }
 
 export default TicketList;
