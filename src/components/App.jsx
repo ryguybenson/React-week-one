ecace3a8ccfdf8ac673b6cbea8ffe135c1628f4c
Riley Benson <ryguybenson@yahoo.com>
2019-04-19 10:53:22 -0700

    create header component and clothes components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
-  return ();
+  return (
+    <div>
+      <Header/>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 export default App;
