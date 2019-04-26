0abbb6156ebf8a6e60ae329c21c3d066ecf37ebb
Riley Benson <ryguybenson@yahoo.com>
2019-04-26 15:51:47 -0700

    adjust header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,25 @@ function Header(){
 
   var header = {
     textAlign: 'center',
-    backgroundImage: 'linear-gradient(to bottom,#282828,#484848,#282828)',
-    position: 'fixed',
-    top: '0',
-    height: '10vh',
     width: '100vw',
-    color: '#f0f0f0'
+    color: '#f0f0f0',
+    marginTop: '4vh'
   };
-
+  var bottomHeader = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  };
   return (
-    <div>
-      <div style={header}>
+    <div style={header}>
+      <div>
         <h1>Tap Room</h1>
+      </div>
+      <div style={bottomHeader}>
+        <h3>Menu</h3>
+        <h3>Events</h3>
+        <h3>About</h3>
+        <h3>contacts</h3>
       </div>
     </div>
   );
